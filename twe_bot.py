d60db2d41c65976c4322324d94595144241d299b
Amit Karnik <karnikamit46@gmail.com>
2015-05-24 09:57:30 +0530

    Update twe_bot.py

--- twe_bot.py
+++ twe_bot.py
@@ -10,8 +10,6 @@ class TweeBot:
         self.app_route = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
         configfile = simpleconfigparser()
         configfile.read(self.app_route+'/twitter_api/api_twitter.ini')
-        # self.screen_name = screen_name
-        # self.base_url = 'https://api.twitter.com/1.1/statuses/update.json?status='
         self.access_token = configfile.login.access_token
         self.access_token_secret = configfile.login.access_token_secret
         self.consumer_key = configfile.login.consumer_key
@@ -21,13 +19,17 @@ class TweeBot:
         self.api = tweepy.API(auth)
 
     def tweet(self):
-        file_open = open('', 'r')
+        file_open = open('', 'r')       # Path to the file which u want to tweet.
         f = file_open.readlines()
         file_open.close()
         for line in f:
-            if len(line) <= 140:
+            if len(line) > 140:
+                line = line[:140]
+            try:
                 self.api.update_status(status=line)
-                time.sleep(3)
+                time.sleep(60)
+            except Exception, e:
+                pass
         print 'done!'
 
 
